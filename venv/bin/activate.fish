cd93e88ec4389a7e70425101cab4edc73127a898
Gabriella <gabriella@u.nus.edu>
2025-02-16 14:13:00 +0800

    Fix some occurences of eryz-os

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/gabriella/Documents/Eryz-Notes/venv"
+set -gx VIRTUAL_ENV "/Users/gabriella/Documents/eryz-os/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
